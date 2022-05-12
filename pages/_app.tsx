8cd0e1fca6bb96a1854eac614c3ce2b3282b6993
Vetle de Vries <vetle1608@gmail.com>
2022-05-12 14:53:42 +0200

    react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    const [queryClient] = useState(() => new QueryClient())
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <Component {...pageProps} />
+            </Hydrate>
+        </QueryClientProvider>
+    )
 }
 
 export default MyApp
