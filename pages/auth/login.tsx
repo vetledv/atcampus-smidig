b4192bce897edbc25293b2217674b455ee7278bd
Vetle de Vries <vetle1608@gmail.com>
2022-05-13 14:39:31 +0200

    login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -5,36 +5,39 @@ import Image from 'next/image'
 
 const Login = ({ providers }: { providers: GetServerSideProps }) => {
     return (
-        <div className='flex flex-col h-screen w-screen justify-center items-center'>
-            <div className='pb-8 -mt-8'>
-                <Image
-                    src={'/atcampus-full-logo.svg'}
-                    width={208}
-                    height={42}
-                    alt='atcampus'
-                />
-            </div>
-            <div className='flex flex-col justify-center items-center rounded-md p-8 bg-white'>
-                <div className='mb-8 text-center'>
-                    <p className='font-bold text-[40px]'>{"Let's go!"}</p>
-                    <p className='text-sm'>
-                        For students. By students. Finally.
-                    </p>
+        <div className='h-screen w-screen p-4'>
+            <div className='w-full max-w-prose text-left sm:mx-auto sm:w-full '>
+                <div className='text-center mt-2 mb-4 lg:mt-8'>
+                    <Image
+                        src={'/atcampus-full-logo.svg'}
+                        width={208}
+                        height={42}
+                        alt='atcampus'
+                    />
                 </div>
-                {providers &&
-                    Object.values(providers).map((provider) => (
-                        <div key={provider.name}>
-                            <FlatButton
-                                as='button'
-                                onClick={() =>
-                                    signIn(provider.id, {
-                                        callbackUrl: `${window.location.origin}/`,
-                                    })
-                                }>
-                                Sign in with {provider.name}
-                            </FlatButton>
-                        </div>
-                    ))}
+                <div className='rounded p-8 bg-white  lg:my-16 lg:py-16 lg:px-20'>
+                    <div className='mb-8 text-center'>
+                        <p className='font-bold text-[40px]'>{"Let's go!"}</p>
+                        <p className='text-sm'>
+                            For students. By students. Finally.
+                        </p>
+                    </div>
+                    {providers &&
+                        Object.values(providers).map((provider) => (
+                            <div key={provider.name}>
+                                <FlatButton
+                                    className={'w-full '}
+                                    as='button'
+                                    onClick={() =>
+                                        signIn(provider.id, {
+                                            callbackUrl: `${window.location.origin}/`,
+                                        })
+                                    }>
+                                    Sign in with {provider.name}
+                                </FlatButton>
+                            </div>
+                        ))}
+                </div>
             </div>
         </div>
     )
