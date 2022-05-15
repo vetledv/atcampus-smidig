f414e11738d4eec40deb85e29e460a662a8e86be
Vetle de Vries <vetle1608@gmail.com>
2022-05-15 21:53:43 +0200

    fix login being visible before redirect when logged in

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,9 +4,19 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function middleware(req: NextRequest, res: NextResponse) {
     const secret_key = process.env.NEXT_AUTH_SECRET
 
+    const session = await getToken({ req, secret: secret_key })
     const url = req.nextUrl.clone()
+
+    //redirect logged in users to the homepage when they try to access login
+    if (url.pathname.startsWith('/auth/')) {
+        if (session) {
+            url.pathname = '/'
+            return NextResponse.redirect(url)
+        }
+    }
+    //redirect non-logged in users to the login page when they try to access pages
     if (url.pathname === '/') {
-        const session = await getToken({ req, secret: secret_key })
+        console.log(session)
         url.pathname = 'auth/login'
         if (!session) return NextResponse.redirect(url)
     }
