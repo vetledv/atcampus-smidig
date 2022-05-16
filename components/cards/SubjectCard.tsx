47c998a691adb320343216f7fd6accf96787921c
Vetle de Vries <vetle1608@gmail.com>
2022-05-16 18:32:36 +0200

    subjectcard cleanup

--- components/cards/SubjectCard.tsx
+++ components/cards/SubjectCard.tsx
@@ -25,38 +25,34 @@ const SubjectCard = ({
     const imageSize = compact ? 64 : 128
 
     return (
-        <>
-            <div
-                onClick={() => router.push(`/groups/${groupName}`)}
-                className='cursor-pointer flex p-3 input-shadow max-w-sm items-center bg-white rounded-standard text-dark-1'>
-                <Image
-                    src={groupImage}
-                    width={imageSize}
-                    height={imageSize}
-                    alt=''
-                />
-                <div className={'px-6'}>
-                    <div className='p-1'>
-                        <div className={'text-lg font-semibold'}>
-                            {groupName}
-                        </div>
-                        <div className='text-sm'>{subjectCode}</div>
-                    </div>
-                    {!compact ? (
-                        <div className='py-1'>
-                            <GroupMembers
-                                members={members}
-                                totalMembers={totalMembers}
-                                color={'dark-1'}
-                            />
-                            <div className={'py-1'}>
-                                <FlatButton>Join Group</FlatButton>
-                            </div>
-                        </div>
-                    ) : null}
+        <div
+            onClick={() => router.push(`/groups/${groupName}`)}
+            className='cursor-pointer flex p-3 input-shadow max-w-sm items-center bg-white rounded-standard text-dark-1'>
+            <Image
+                src={groupImage}
+                width={imageSize}
+                height={imageSize}
+                alt=''
+            />
+            <div className={'px-6'}>
+                <div className='p-1'>
+                    <div className={'text-lg font-semibold'}>{groupName}</div>
+                    <div className='text-sm'>{subjectCode}</div>
                 </div>
+                {!compact ? (
+                    <div className='py-1'>
+                        <GroupMembers
+                            members={members}
+                            totalMembers={totalMembers}
+                            color={'dark-1'}
+                        />
+                        <div className={'py-1'}>
+                            <FlatButton>Join Group</FlatButton>
+                        </div>
+                    </div>
+                ) : null}
             </div>
-        </>
+        </div>
     )
 }
 
