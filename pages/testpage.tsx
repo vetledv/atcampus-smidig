eb03f731b7ae6346d23483f1e34a4bb133d5f7f7
tommenbro <74247764+tommenbro@users.noreply.github.com>
2022-05-16 15:17:23 +0200

    edit group card

--- pages/testpage.tsx
+++ pages/testpage.tsx
@@ -1,4 +1,5 @@
 import AddTag from 'components/groups/AddTag'
+import EditTagCard from 'components/groups/EditGroupCard'
 import NavBarTut from 'components/navigation/NavBarTutorial'
 import NavBarTut2 from 'components/navigation/NavBarTutorial2'
 import React from 'react'
@@ -10,11 +11,13 @@ const TestPage = () => {
             <NavBarTut2 />
             <div className='bg-red-800'>
                 <AddTag />
+                <EditTagCard />
                 <div className='flex flex-col items-center justify-center'>
                     <h1 className='text-3xl font-bold'>TestPage</h1>
                     <p className='text-xl my-36 space-y-36'>
                         This is a test page for the tutorial.
                     </p>
+
                     <h1 className='text-3xl font-bold'>TestPage</h1>
                     <p className='text-xl py-36 space-y-36'>
                         This is a test page for the tutorial.
