080dd3dcc2d9baae2f0e2e4810cf1e4bff061840
JensHjelset <jehj002@egms.no>
2022-05-19 16:35:28 +0200

    choosegoal mal side

---
+++ pages/choosegoal.tsx
@@ -0,0 +1,11 @@
+import FindGroupsHeader from 'components/findgroups/FindGroupsHeader'
+
+const ChooseGoalPage = () => {
+    return (
+        <>
+            <div>Test Push</div>
+        </>
+    )
+}
+
+export default ChooseGoalPage
