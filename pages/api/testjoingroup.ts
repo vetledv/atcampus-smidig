247846e2229cf6d6b088a1b977f4839bd54e0613
Vetle de Vries <vetle1608@gmail.com>
2022-05-19 16:10:01 +0200

    testjoinggorup

--- pages/api/testjoingroup.ts
+++ pages/api/testjoingroup.ts
@@ -2,38 +2,53 @@ import { connectToDB } from 'lib/mongodb'
 import { ObjectId } from 'mongodb'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(
+export default async function handlera(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     const { db } = await connectToDB()
-    const { groupId, userId, userName } = JSON.parse(req.body)
 
     //TODO: if group is private, add user to pendingMembers instead of members
     //TODO: if group is full, send error message
-
-    const filter = {
-        _id: new ObjectId(groupId),
-    }
-    const update = {
-        $addToSet: {
-            members: {
-                userId: userId,
-                userName: userName,
-            },
-        },
-    }
-
-    await db
-        .collection('atcampus-groups')
-        .updateOne(filter, update)
-        .then(() => {
-            res.status(200).send('success')
-        })
-        .catch((err) => {
-            res.status(500).json({
-                message: 'error',
-                error: err,
+    if (req.method === 'GET') {
+        await db
+            .collection('atcampus-groups')
+            .find({})
+            .toArray()
+            .then((groups) => {
+                if (groups.length > 0) {
+                    res.status(200).json(groups)
+                } else {
+                    res.status(200).json([])
+                }
             })
-        })
+    }
+
+    if (req.method === 'POST') {
+        const { groupId, userId, userName } = req.body
+        const filter = {
+            _id: new ObjectId(groupId),
+        }
+        const update = {
+            $addToSet: {
+                members: {
+                    userId: userId,
+                    userName: userName,
+                },
+            },
+        }
+
+        await db
+            .collection('atcampus-groups')
+            .updateOne(filter, update)
+            .then(() => {
+                res.status(200).send('success')
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    message: 'error',
+                    error: err,
+                })
+            })
+    }
 }
