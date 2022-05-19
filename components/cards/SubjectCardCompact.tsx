8d4f828bf141f09104a7f4f05c954842a4eb7ab0
Magnus Soleim <magnus@soleim.com>
2022-05-19 14:20:24 +0200

    compact card for subjects only, not groups. temporary

---
+++ components/cards/SubjectCardCompact.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const SubjectCardCompact = ({
+    subjectImage,
+    subjectName,
+    subjectCode,
+    subjectId,
+}) => {
+    return (
+        <>
+            {' '}
+            <Link href={`/${subjectId}`}>
+                <div className='flex p-3 input-shadow w-96 items-center bg-white rounded-standard text-dark-1'>
+                    <Image src={subjectImage} width={64} height={64} alt='' />
+                    <div className={'px-6'}>
+                        <div className='p-1'>
+                            <div className={'text-lg font-semibold'}>
+                                {subjectName}
+                            </div>
+                            <div className={'text-sm'}>{subjectCode}</div>
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        </>
+    )
+}
+
+export default SubjectCardCompact
