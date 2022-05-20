f43f569102aa4e056d5bfb07eb5c5bacf98a7f9d
tommenbro <74247764+tommenbro@users.noreply.github.com>
2022-05-20 13:29:55 +0200

    Minor fix for find new group button

--- components/findgroups/TwoOptionsButtons.tsx
+++ components/findgroups/TwoOptionsButtons.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { PlusCircleIcon } from '@heroicons/react/outline'
 const TwoOptionsButtons = () => {
     return (
-        <div className='flex justify-center bg-white p-3 w-[300px] h-[80px] border-dark-5 rounded-md'>
+        <div className='flex justify-center bg-white p-3 w-[300px] h-[80px] border-dark-5 rounded-md hover:shadow-md'>
             {/*onClick={'/findgroup'}*/}
             <div className=' items-center text-center justify-center '>
                 <PlusCircleIcon className='text-dark-3 w-8 ml-8 ' />
-                <h1 className='text-3xl text-dark-3  text-center '>
+                <h1 className='text-5xl text-dark-3  text-center '>
                     Finn ny gruppe
                 </h1>
             </div>
