53d20f219c018f13a44c0e6f3d01e7553b2b6fa3
tommenbro <74247764+tommenbro@users.noreply.github.com>
2022-05-20 15:59:01 +0200

    GroupNav almost donezy

--- pages/testpage.tsx
+++ pages/testpage.tsx
@@ -1,7 +1,9 @@
 import TwoOptionsButtons from 'components/findgroups/TwoOptionsButtons'
+import GroupHeader from 'components/groups/GroupHeaderMobile'
 import AddTag from 'components/groups/GroupSettings/AddTag'
 import EditTagCard from 'components/groups/GroupSettings/EditGroupCard'
 import GroupMembers from 'components/groups/GroupSettings/GroupMembers'
+import GroupNav from 'components/groups/GroupSettings/GroupNav'
 import NavBarTut from 'components/navigation/NavBarTutorial'
 import NavBarTut2 from 'components/navigation/NavBarTutorial2'
 import React from 'react'
@@ -18,6 +20,7 @@ const TestPage = () => {
                     <TwoOptionsButtons />
                 </div>
                 <GroupMembers />
+                <GroupNav />
                 <div className='flex flex-col items-center justify-center'>
                     <h1 className='text-3xl font-bold'>TestPage</h1>
                     <p className='text-xl my-36 space-y-36'>
