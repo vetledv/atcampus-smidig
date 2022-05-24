cc55d5f44bd9ea14f55689b4a0931b33dbe8ae1d
ande29 <44792983+ande29@users.noreply.github.com>
2022-05-24 16:55:08 +0200

    progressbar update
    stilsatt og clickable

---
+++ components/findgroups/ProgressBarTab.tsx
@@ -0,0 +1,33 @@
+import { CheckIcon } from '@heroicons/react/outline'
+
+const ProgressBarTab = ({
+    onClick,
+    step,
+    value,
+    active,
+    completed
+}) => {
+    const tab = 'flex rounded-full bg-purple-4 w-7 h-7 justify-center items-center text-white'
+    const activeTab = 'flex bg-purple-1 rounded-full text-white w-10 h-10 justify-center items-center'
+    const completedTab = 'flex rounded-full bg-purple-1 text-white w-7 h-7 justify-center items-center'
+
+  return (
+    <div className='flex items-center'>
+        <div 
+            className={
+                (active ? activeTab : completed ? completedTab : tab)
+            }
+            onClick={onClick}>
+            <span className={value > step+1 ? 'invisible' : ''}>
+                {value === 4 ? <CheckIcon className='w-7 h-7'/> : value}
+            </span>
+        </div>
+
+        {value !< 4 && (
+            <div className={step >= value ? 'bg-purple-1 h-1 w-8' : 'bg-purple-4 h-1 w-8'}></div>        
+        )}
+    </div>
+  )
+}
+
+export default ProgressBarTab
--- components/findgroups/ProgressBarTest.tsx
+++ components/findgroups/ProgressBarTest.tsx
@@ -1,43 +1,27 @@
-import { CheckIcon } from '@heroicons/react/outline'
-const ProgressBarTest = ({ step }) => {
-    const tab =
-        'flex rounded-full bg-purple-4 w-7 h-7 justify-center items-center'
-    const activeTab =
-        'flex rounded-full bg-purple-1 text-white w-10 h-10 justify-center items-center'
+import ProgressBarTab from './ProgressBarTab'
 
-    return (
-        <div className='flex flex-row w-full justify-center items-center mt-2'>
-            <div className={step >= 0 ? activeTab : tab}>
-                <span>1</span>
-            </div>
 
-            <div
-                className={
-                    step > 0 ? 'bg-purple-1 h-1 w-8' : 'bg-purple-4 h-1 w-8'
-                }></div>
-            <div className={step >= 1 ? activeTab : tab}>
-                <span className={step < 1 ? 'invisible' : ''}>2</span>
-            </div>
-
-            <div
-                className={
-                    step > 1 ? 'bg-purple-1 h-1 w-8' : 'bg-purple-4 h-1 w-8'
-                }></div>
-            <div className={step >= 2 ? activeTab : tab}>
-                <span className={step < 2 ? 'invisible' : ''}>3</span>
-            </div>
-
-            <div
-                className={
-                    step > 2 ? 'bg-purple-1 h-1 w-8' : 'bg-purple-4 h-1 w-8'
-                }></div>
-            <div className={step >= 3 ? activeTab : tab}>
-                <span className={step < 3 ? 'invisible' : 'w-7 h-7'}>
-                    {<CheckIcon />}
-                </span>
-            </div>
-        </div>
-    )
+const ProgressBarTest = ({ 
+    step,
+    onClick
+}) => {
+    const steps = [1, 2, 3, 4]
+    console.log('step: ' + step)
+  return (
+    <div className="flex flex-row w-full justify-center items-center mt-2">
+        
+        {steps.map((e) => (
+            <ProgressBarTab
+            key={e} 
+            onClick={onClick}
+            step={step}
+            value={e}
+            active={e === step+1}
+            completed={e < step+1}
+        />
+        ))}
+    </div>
+  )
 }
 
 export default ProgressBarTest
