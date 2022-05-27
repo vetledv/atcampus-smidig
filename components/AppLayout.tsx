b00cc4e1b2a42337bc2049437a0e685f596d16c1
Magnus Soleim <magnus@soleim.com>
2022-05-27 13:52:09 +0200

    Fixed media queries in header/menu. Dropdown fix, dropdown state fix

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -8,7 +8,7 @@ const AppLayout = ({ children }) => {
             <Menu />
             <div className='xl:pl-[18rem] bg-gray-50'>
                 <Header />
-                <div className='w-full min-h-screen lg:mt-[91px] flex flex-col mb-20 lg:mb-0'>
+                <div className='w-full min-h-screen xl:mt-[91px] flex flex-col'>
                     {children}
                 </div>
                 <Footer />
--- components/general/DropDown.tsx
+++ components/general/DropDown.tsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react'
 import onClickOutside, { HandleClickOutside } from 'react-onclickoutside'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 
-function DropDown({ title = undefined, items, dropDownTitle }) {
+function DropDown({
+    title = undefined,
+    items,
+    dropDownTitle,
+    setSelectedSchool,
+}) {
     const [open, setOpen] = useState(false) // dropdown is open or not
     const [selection, setSelection] = useState()
     const toggle = () => setOpen(!open) // toggle dropdown
@@ -11,11 +16,12 @@ function DropDown({ title = undefined, items, dropDownTitle }) {
     const handleOnClick = (item) => {
         setSelection(item) //When an item in DD is clicked it is added to the selection. Use this to render the selected items.
         toggle() //Closes the dropdown
+        setSelectedSchool(item.value) //Sets the selected school in the parent component
     }
 
     return (
         /*May need to resize*/
-        <div className='rounded-standard border-2 border-solid border-dark-5 min-w-fit max-w-screen-lg w-4/5 bg-white'>
+        <div className='rounded-standard border-2 border-solid border-dark-5 min-w-fit max-w-screen-lg md:w-1/3 xl:w-1/4 bg-white absolute'>
             <div tabIndex={0} onClick={() => toggle()}>
                 <div className='flex justify-between px-6 cursor-pointer items-center'>
                     <div className='font-semibold text-lg text-purple-1 '>
@@ -31,7 +37,9 @@ function DropDown({ title = undefined, items, dropDownTitle }) {
                             <div
                                 onClick={() => handleOnClick(item)}
                                 className={'w-full'}>
-                                <div className='px-3 py-1 cursor-default text-md hover:bg-purple-5 bg-white rounded-standard'>
+                                <div
+                                    className='px-3 py-1 cursor-default text-md hover:bg-purple-5 bg-white rounded-standard'
+                                    onClick={handleOnClick}>
                                     {item.value}
                                 </div>
                             </div>
