90e7e4bc4377b0807cc9ed4602a903dd4fd7bb5d
Magnus Soleim <magnus@soleim.com>
2022-05-30 11:23:27 +0200

    refactor svgs for more code readability

--- components/findgroups/ChooseGroup.tsx
+++ components/findgroups/ChooseGroup.tsx
@@ -8,8 +8,6 @@ import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { useMutation } from 'react-query'
 
-//TODO:       Highlight matching tags!!!!!!  Highlight matching tags!!!!!!  Highlight matching tags!!!!!!  Highlight matching tags!!!!!!
-
 const ChooseGroup = ({
     search,
     refetch,
@@ -93,16 +91,7 @@ const ChooseGroup = ({
                         type='button'
                         onClick={() => setShowModal(false)}
                         className='absolute -mt-4 right-0 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm inline-flex items-center '>
-                        <svg
-                            className='w-5 h-5'
-                            fill='currentColor'
-                            viewBox='0 0 20 20'
-                            xmlns='http://www.w3.org/2000/svg'>
-                            <path
-                                fillRule='evenodd'
-                                d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
-                                clipRule='evenodd'></path>
-                        </svg>
+                        {groupSvgs.exitModal}
                     </button>
                     <div className='flex h-fit justify-between'>
                         <p className='text-2xl font-semibold'>
@@ -112,19 +101,7 @@ const ChooseGroup = ({
                             {group.private ? (
                                 <div className='flex'>
                                     <p className=''>Privat gruppe</p>
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        className='h-6 w-6'
-                                        fill='none'
-                                        viewBox='0 0 24 24'
-                                        stroke='currentColor'
-                                        strokeWidth={1.5}>
-                                        <path
-                                            strokeLinecap='round'
-                                            strokeLinejoin='round'
-                                            d='M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z'
-                                        />
-                                    </svg>
+                                    {groupSvgs.privateGroup}
                                 </div>
                             ) : (
                                 <div className='hidden'></div>
@@ -337,3 +314,34 @@ const ChooseGroup = ({
 }
 
 export default ChooseGroup
+
+const groupSvgs = {
+    exitModal: (
+        <svg
+            className='w-5 h-5'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'>
+            <path
+                fillRule='evenodd'
+                d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+                clipRule='evenodd'></path>
+        </svg>
+    ),
+    privateGroup: (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            strokeWidth={1.5}>
+            <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z'
+            />
+        </svg>
+    ),
+    '3': '/static/svg/group-3.svg',
+}
