07beb1ad8e42d7d726fcd88c67f116e0c5ef1a66
Janixern <78200060+JanixernSkole@users.noreply.github.com>
2022-05-31 13:19:33 +0200

    home style

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const HomePage: NextPage = () => {
     return (
         <>
             <h1>Oversikt</h1>
-            <div className='grid grid-cols-2 gap-12 md:grid-cols-8 md:gap-2 lg:text-center lg:grid-col-10 lg:gap-5 '>
+            <div className='pr-4 grid grid-cols-2 gap-12 md:grid-cols-8 md:gap-2 lg:text-center lg:grid-col-10 lg:gap-5 '>
             <FlatButton className='bg-logout-red flex col-start-6 col-end-6 col col-span-2 justify-center lg:text-center' onClick={() => signOut()}>Logg ut</FlatButton></div>
             <>dont add shit here</>
         </>
